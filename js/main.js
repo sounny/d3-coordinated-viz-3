1743ed212b5586b8ab694d4a21f771dc57494672
CoraBJohnson <111707632+CoraBJohnson@users.noreply.github.com>
2022-11-21 21:53:41 -0600

    add background states

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+//begin script when window loads
+window.onload = setMap();
 
+//set up choropleth map
+function setMap(){
+    //use queue to parallelize asynchronous data loading
+    d3.queue()
+        .defer(d3.csv, "data/NC_janData.csv") //load attributes from csv
+        .defer(d3.json, "data/nc_counties.topojson") //load choropleth spatial data
+}

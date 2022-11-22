c83a85b977bcb41defcdcf9a2ffa1dc7576011ae
CoraBJohnson <111707632+CoraBJohnson@users.noreply.github.com>
2022-11-21 22:29:31 -0600

    Promise.all to load data

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 //begin script when window loads
 window.onload = setMap();
 
+//array for data file names
+const files = ['NC_janData.csv', 'nc_counties.topojson', 'backgroundStates.topojson']
+
 //set up choropleth map
 function setMap(){
-    //use queue to parallelize asynchronous data loading
-    d3.queue()
-        .defer(d3.csv, "data/NC_janData.csv") //load attributes from csv
-        .defer(d3.json, "data/nc_counties.topojson") //load choropleth spatial data
-}
+    //use promise.all to parallelize asynchronous data loading
+    var promises = [];
+    promises.push(d3.csv('data/NC_janData.csv'));    // load attributes from csv
+    promises.push(d3.json('data/nc_counties.topojson'));  //load choropleth spatial data
+    promises.push(d3.json, 'data/backgroundStates.topojson');  //load background spatial data
+    Promise.all(promises).then(callback);
+
+    function callback(data){
+        csvData = data[0];
+        nc = data[1];
+        states = data[2];
+        console.log(csvData);
+        console.log(nc);
+        console.log(states);
+
+    };
+
+};

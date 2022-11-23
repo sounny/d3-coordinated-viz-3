fc9f3405581ac7c2b61fae937879f0abcc925a4f
CoraBJohnson <111707632+CoraBJohnson@users.noreply.github.com>
2022-11-22 21:40:06 -0600

    struggling with topojson undefined type error
    Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'type')
        at Object.S [as feature] (topojson.min.js:2:5551)
        at callback (main.js:46:35)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 //begin script when window loads
 window.onload = setMap();
 
-
 //set up choropleth map
-function setMap(){
+function setMap() {
 
     //map frame dimensions
-    var width=960,
-        height=460;
+    var width = 960,
+        height = 460;
 
     //create new svg container for the map
     var map = d3.select('body')
@@ -18,11 +17,11 @@ function setMap(){
 
     //create Albers equal area conic projection centered on North Carolina
     var projection = d3.geoAlbers()
-        .center([0,35.7])
+        .center([1.2, 35.3])
         .rotate([81, 0, 0])
-        .parallels([29.5,45.5])
-        .scale(2700)
-        .translate([width/2, height/2]);
+        .parallels([29.5, 45.5])
+        .scale(7000)
+        .translate([width / 2, height / 2]);
     //console.log(map);
 
     //create path generator
@@ -32,20 +31,21 @@ function setMap(){
     //use promise.all to parallelize asynchronous data loading
     var promises = [];
     promises.push(d3.csv('data/NC_janData.csv'));    // load attributes from csv
-    promises.push(d3.json('data/nc_counties.topojson'));  //load choropleth spatial data
     promises.push(d3.json('data/backgroundStates.topojson'));  //load background spatial data
+    promises.push(d3.json('data/nc_counties.topojson'));  //load choropleth spatial data
     Promise.all(promises).then(callback);
+    //console.log(promises);
 
-    function callback(data){
+    function callback(data) {
 
         csvData = data[0];
         nc = data[1];
         states = data[2];
 
         //translate topoJSONs
-        var ncCounties = topojson.feature(nc, nc.objects.ncCounties);     //assign variable names to the features in the topojson data
-        var stateOutlines = topojson.feature(states, states.objects.stateOutlines).features; //get array of features to pass to .data()
-        console.log(ncCounties);
+        var ncCounties = topojson.feature(nc, nc.objects.ncCounties),     //assign variable names to the features in the topojson data
+            stateOutlines = topojson.feature(states, states.objects.stateOutlines).features; //get array of features to pass to .data()
+       console.log(ncCounties);
 
 
         //add states to map using path generator. creates single svg element for states
@@ -59,11 +59,9 @@ function setMap(){
             .data(ncCounties)
             .enter()
             .append('path')
-            .attr('class', function(d){
-                return"counties"+ d.properties.NAME_ALT;
+            .attr('class', function (d) {
+                return d.properties.NAME_ALT;
             })
             .attr("d", path);
-    };
-
-
+    }
 };
